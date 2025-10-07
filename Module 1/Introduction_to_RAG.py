d722340ac736d1effb490d6c70fd8129530c82a6
suportesupernet <suportesupernet@outlook.com>
2025-10-07 17:20:21 -0300

    Finished Introduction_to_RAG.py

--- Module 1/Introduction_to_RAG.py
+++ Module 1/Introduction_to_RAG.py
@@ -38,4 +38,117 @@ print('file downloaded')
 with open(filename, 'r') as file:
     contents = file.read()
     print(contents)
+loader = TextLoader(filename)
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(documents)
+print(len(texts))
 
+embeddings = HuggingFaceEmbeddings()
+docsearch = Chroma.from_documents(texts, embeddings)
+print('document ingested')
+
+model_id = 'google/flan-t5-xl'
+parameters = {
+    GenParams.DECODING_METHOD: DecodingMethods.GREEDY,
+    GenParams.MIN_NEX_TOKENS: 130,
+    GenParams.MAX_NEW_TOKENS: 256,
+    GenParams.TEMPERATURE: 0.5
+}
+
+credentials = {
+    "url": "url",
+    "api_key": "key"
+}
+
+model = Model(
+    model_id=model_id,
+    params=parameters,
+    credentials=credentials,
+    project_id=project_id
+)
+
+flan_llm = WatsonLLM(model=model)
+
+qa = RetrievalQA.from_chain_type(llm=flan_llm,chain_type="stuff",retriever=docsearch.as_retriever(),
+                                 return_sourse_documents=False)
+query= "what is mobile policy?"
+qa.invoke(query)
+
+------------------------------------------------------
+model_id = 'meta-llama/llama-3-3-70b-instruct'
+
+parameters = {
+    GenParams.DECODINGMETHOD: DecodingMethods.GREEDY,
+    GenParams.MAX_NEW_TOKENS: 256,
+    GenParams.TEMPERATURE: 0.5
+}
+
+credentials = {
+    "url": "url"
+}
+project_id = "project"
+
+model = Model(
+    model_id=model_id,
+    params=parameters,
+    credentials=credentials,
+    project_id=project_id
+)
+
+llama_llm = WatsonxLLM(model=model)
+
+qa = RetrievalQA.from_chain_type(llm=llama_llm, chain_type="stuff",
+                                 retriever=docsearch.as_retriever(),
+                                 return_source_documents=False)
+query = "Can you summarize the document for me?"
+qa.invoke(query)
+
+prompt_template = """Use the information from the document to answer the question at the end. If you don't know the answer, just say that you don't know, definately do not try to make up an answer.
+{context}
+Question: {Question}
+"""
+
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+
+chain_type_kwargs = {"prompt": PROMPT}
+
+qa = RetrievalQA.from_chain_type(llm=llama_llm, chain_type="stuff", retriever=docsearch.as_retriever(),
+                                 chain_type_kwargs=chain_type_kwargs,
+                                 return_source_documents=False)
+query = "Can i eat in company vehicles?"
+qa.invoke(query)
+
+memory = ConversationBufferMemory(memory_key = "chat_history", return_message = True)
+qa = ConversationalRetrievalChain.from_llm(llm=llama_llm, chain_type="stuff",
+                                           retriever=docsearch.as_retriever(),
+                                           memory=memory,
+                                           get_chat_history=lambda h : h,
+                                           return_resource_documents=False)
+history = []
+query = "What is mobile policy?"
+result = qa.invoke({"question":query}, {"chat_history":history})
+print(result["answer"])
+history.append((query, result["answer"]))
+result = qa({"question": query}, {"chat_history": history})
+
+# An Agent
+def qa():
+    memory = ConversationBufferMemory(memory_key = "chat_history", return_message = True)
+    qa = ConversationalRetrievalChain.from_llm(llm=llama_llm, chain_type="stuff",
+    retriever=docsearch.as_retriever(), memory = memory, get_chat_history=lambda h:h,
+    return_source_documents=False)
+
+    history = []
+    while True:
+        query = input("Question: ")
+
+        if query.lower() in ["quit", "exit", "bye"]:
+            print("Answer: Goodbye!")
+            break
+
+        result = qa({"question": query}, {"chat_history": history})
+        history.append((query, result["answer"]))
+        print("Answer: ", result["answer"])
